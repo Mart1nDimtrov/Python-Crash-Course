a166aee59976350ec083dca40bc9fabac855ad45
giggly <giggly@abv.bg>
2020-02-28 23:22:20 +0200

    Working with strings!

---
+++ 02. Variables and Simple Data Types/famous_quote_2.py
@@ -0,0 +1,10 @@
+# 2-6. Famous Quote 2:Repeat Exercise 2-5, but this time, represent the 
+# famous person’s name using a variable called famous_person. Then compose 
+# your  message and represent it with a new variable called message. Print your 
+# message.
+
+famous_person = "Albert Einstein"
+quote = "A person who never made a mistake never tried anything new."
+message = f"{famous_person} once said,\n{quote}"
+
+print(message)
