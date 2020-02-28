2d62496431e533d52df4e3fa9cc2ce0afb550816
giggly <giggly@abv.bg>
2020-02-28 07:57:42 +0200

    Naming variables

---
+++ 02. Variables and Simple Data Types/simple_messages.py
@@ -0,0 +1,8 @@
+# 2-2. Simple Messages:Assign a message to a variable, and print that message. 
+# Then change the value of the variable to a new message, and print the new 
+# message.
+message = "A simple message"
+print(message)
+message = "Another simple message"
+print(message)
+
