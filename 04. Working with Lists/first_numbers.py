469f706f80c3e9ba62166dd390bca4a974adb5ca
giggly <giggly@abv.bg>
2020-03-06 23:41:40 +0200

    Generating numbers for lists.

---
+++ 04. Working with Lists/first_numbers.py
@@ -0,0 +1,12 @@
+for value in range(1, 5):
+	print(value)
+
+print("\n")
+# pass range only one argument
+for value in range(5):
+	print(value)
+
+print("\n")
+# use list() to have the numbers
+numbers = list(range(5))
+print(numbers)
