d8054d045f905133c3ade481ca9c9c2a7095d321
Mart1nDimtrov <sly_racoon88@abv.bg>
2020-03-08 23:53:39 +0200

    Working with slices.

---
+++ 04. Working with Lists/players.py
@@ -0,0 +1,10 @@
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print(players[0:3])
+# start with beginning till specified
+print(players[:2])
+# start with specified till end
+print(players[2:])
+# start from the back
+print(players[-3:])
+# skip values
+print(players[0:5:2])
---
+++ 04. Working with Lists/slices.py
@@ -0,0 +1,25 @@
+# 4-10. Slices: Using one of the programs you wrote in this chapter,
+# add several lines to the end of the program that do the following:
+#    Print the message The first three items in the list are:.
+#        Then use a slice to print the first three items from that program’s list.
+#    Print the message Three items from the middle of the list are:.
+#        Use a slice to print three items from the middle of the list.
+#    Print the message The last three items in the list are:.
+#        Use a slice to print the last three items in the list. 
+
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+
+print("The first three items in the list are:")
+for player in players[:3]:
+    print(player.title())
+
+print("The last three items in the list are:")
+for player in players[-3:]:
+    print(player.title())
+
+# get the middle
+middle = len(players) // 2
+print(middle)
+print("Three items from the middle of the list are:")
+for player in players[middle-1:middle+2]:
+    print(player.title())
