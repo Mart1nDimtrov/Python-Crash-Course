b7a8d035baea1a19e13bcc2a961b926edce596e5
giggly <giggly@abv.bg>
2020-05-18 21:30:39 +0300

    Learning about the if statement.

---
+++ 05. if Statements/banned_users.py
@@ -0,0 +1,5 @@
+banned_users = ['david','john','casie']
+user = 'marie'
+
+if user not in banned_users:
+	print(f'{user.title()}, you can add a comment if you like. :)')
---
+++ 05. if Statements/magic_number.py
@@ -0,0 +1,4 @@
+guess = 17
+
+if guess != 42:
+	print("That is not the correct answer. Please try again!")
