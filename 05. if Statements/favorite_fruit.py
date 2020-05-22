dcce84239ecd7108a86b4c78955cefb1a139d159
giggly <giggly@abv.bg>
2020-05-22 21:32:34 +0300

    Diving deeper into conditional statements.

---
+++ 05. if Statements/favorite_fruit.py
@@ -0,0 +1,23 @@
+#5 - 7. Favorite Fruit:Make a list of your favorite fruits, and then write a series of 
+#independent if statements that check for certain fruits in your list.
+#•	 Make a list of your three favorite fruits and call it favorite_fruits.
+#•	 Write five ifstatements. Each should check whether a certain kind of fruit 
+#is in your list. If the fruit is in your list, the if block should print a statement, 
+#such as You really like bananas!
+
+favorite_fruits = ['strawberry','apple','watermelon']
+
+if 'watermelon' in favorite_fruits:
+	print('You really like watermelon!')
+
+if 'apple' in favorite_fruits:
+	print('You really like apple!')
+
+if 'strawberry' in favorite_fruits:
+	print('You really like strawberry!')
+
+if 'honeydew' in favorite_fruits:
+	print('You really like honeydew!')
+
+if 'pineapple' in favorite_fruits:
+	print('You really like pineapple!')
