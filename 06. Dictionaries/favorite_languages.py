1e628ad1bd62638c75641a7414730cbb41464b12
giggly <giggly@abv.bg>
2020-05-26 22:50:56 +0300

    Looping through dictionaries. Part 1.

--- 06. Dictionaries/favorite_languages.py
+++ 06. Dictionaries/favorite_languages.py
@@ -5,5 +5,14 @@ favorite_languages = {
 	'phil': 'python',
 }
 
-language = favorite_languages['sarah'].title()
-print(f'Sara''s favorite language is ' + language + '.')
+friends = ['phil', 'sarah']
+
+for name,language in sorted(favorite_languages.items()):
+	print(f"{name.title()}'s favorite language is {language.title()}.")
+	#check if the person is a friend
+	if name in friends:
+		print(f'{name.title()}, I see you love {language.title()}!')
+
+#check if Erin is in the poll
+if 'erin' not in favorite_languages.keys(): #.keys() returns a list
+	print("Erin, please take our poll!")
