b6dd3d952b7ad7feff06ea4854ef4901e116f97a
giggly <giggly@abv.bg>
2020-06-05 21:13:21 +0300

    Learning how to exit a loop.

--- 07. User Input and while Loops/while_parrot.py
+++ 07. User Input and while Loops/while_parrot.py
@@ -1,8 +1,10 @@
 message = "Tell me something, and I will repeat it back to you:"
 message += "\nType 'quit' if you want to exit. "
 
-word = ''
-while word != 'quit':
+active = True
+while active:
 	word = input(message)
 	if word != 'quit':
 		print(word)
+	else:
+		active = False
