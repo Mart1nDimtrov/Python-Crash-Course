b45c4e4e4206c905463ce49eef6ac32e40a1441c
giggly <giggly@abv.bg>
2020-06-10 22:19:42 +0300

    Using keyword arguments.

--- 08. Functions/pets.py
+++ 08. Functions/pets.py
@@ -3,8 +3,9 @@ def describe_pet(animal_type, pet_name):
 	print(f"\nI have a {animal_type}.")
 	print(f"My {animal_type}'s name is {pet_name.title()}.")
 
-describe_pet('hamster', 'harry')
-describe_pet('dog', 'willie')
+#rewriting using keyword arguments
+describe_pet(animal_type='hamster', pet_name='harry')
+describe_pet(animal_type='dog', pet_name='willie')
 
-#funny, but wrong
-describe_pet('harry','hamster')
+#equivalent to first one
+describe_pet(pet_name='harry',animal_type='hamster')
