8057852ce5eea20f57e8f77b3101f7f3361aaa99
giggly <giggly@abv.bg>
2020-06-23 23:53:17 +0300

    Mixing positional and arbitrary arguments.

--- 08. Functions/pizza.py
+++ 08. Functions/pizza.py
@@ -1,8 +1,8 @@
-def make_pizza(*toppings):
+def make_pizza(size,*toppings):
 	"""Print the list of toppings that have been requested."""
-	print("\nMaking a pizza with the following toppings:")
+	print(f"\nMaking a {size}-inch pizza with the following toppings:")
 	for topping in toppings:
 		print(f'- {topping}')
 
-make_pizza('pepperoni')
-make_pizza('mushrooms', 'green peppers', 'extra cheese')
+make_pizza(16,'pepperoni')
+make_pizza(18,'mushrooms', 'green peppers', 'extra cheese')
