000e86794a10496702b4863977a6d18f47295e78
giggly <giggly@abv.bg>
2020-07-07 20:27:34 +0300

    Modifying attribute values.

--- 09. Classes/car.py
+++ 09. Classes/car.py
@@ -16,6 +16,42 @@ class Car:
 		"""Print a statement showing the car's mileage."""
 		print(f"This car has {self.odometer_reading} miles on it.")
 
+	def update_odometer(self, mileage):
+		"""
+		Set the odometer reading to the given value.
+		Reject the change if it attempts to roll the odometer back.
+		"""
+		if mileage >= self.odometer_reading:
+			self.odometer_reading = mileage
+		else:
+			print("You can't roll back an odometer!")
+
+	def increment_odometer(self, miles):
+		"""
+		Increment the odometer reading to the given value.
+		Reject the change if it attempts to roll the odometer back.
+		"""
+		if miles >= 0:
+			self.odometer_reading += miles
+		else:
+			print("You can't add negative miles to an odometer!")
+
 my_new_car = Car('audi', 'a4', 2019)
 print(my_new_car.get_descriptive_name())
+
+#set the value of the odometer_reading attribute directly 
+my_new_car.odometer_reading = 23
 my_new_car.read_odometer()
+
+#set the value of the odometer_reading attribute through a method 
+my_new_car.update_odometer(22)
+my_new_car.update_odometer(24)
+my_new_car.read_odometer()
+
+my_used_car = Car('subaru', 'outback', 2015)
+print(my_used_car.get_descriptive_name())
+my_used_car.update_odometer(23_500)
+my_used_car.read_odometer()
+my_used_car.increment_odometer(-100)
+my_used_car.increment_odometer(100)
+my_used_car.read_odometer()
