cce00868422491ce27ec7ed2fa61a85d62ba1d9e
giggly <giggly@abv.bg>
2020-07-11 22:06:52 +0300

    Using instances as attributes.

--- 09. Classes/electric_car.py
+++ 09. Classes/electric_car.py
@@ -36,18 +36,37 @@ class Car:
 		else:
 			print("You can't add negative miles to an odometer!")
 
-class ElectricCar(Car):
-	"""Represent aspects of a car, specific to electric vehicles."""
-	def __init__(self, make, model, year):
-		"""Initialize attributes of the parent class."""
-		super().__init__(make, model, year)
-		self.battery_size = 75
+class Battery:
+	"""Represents a battery of an electric car."""
+	def __init__(self, battery_size=75):
+		"""Initialize the battery's attributes."""
+		self.battery_size = battery_size
 
 	def describe_battery(self):
 		"""Print a statement describing the battery size."""
 		print(f"This car has a {self.battery_size}-kWh battery.")
 
+	def get_range(self):
+		"""Print a statement about the range this battery provides."""
+		if self.battery_size == 75:
+			range = 260
+		elif self.battery_size == 100:
+			range = 310
+
+		print(f"This car can go about {range} miles on a full charge.")
+		
+
+class ElectricCar(Car):
+	"""Represent aspects of a car, specific to electric vehicles."""
+	def __init__(self, make, model, year):
+		"""Initialize attributes of the parent class."""
+		super().__init__(make, model, year)
+		self.battery = Battery()
+
 
 my_tesla = ElectricCar('tesla', 'model s', 2019)
 print(my_tesla.get_descriptive_name())
-my_tesla.describe_battery()
+my_tesla.battery.describe_battery()
+#change the site of the battery
+my_tesla.battery.battery_size = 100
+my_tesla.battery.get_range()
