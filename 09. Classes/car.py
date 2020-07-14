22fdc250cf9b49572815b0a6a0a02f9e2027863e
giggly <giggly@abv.bg>
2020-07-14 22:50:35 +0300

    Importing my first class.

--- 09. Classes/car.py
+++ 09. Classes/car.py
@@ -1,3 +1,4 @@
+"""A class that can be used to represent a car."""
 class Car:
 	"""A simple attempt to represent a car."""
 	def __init__(self, make, model, year):
@@ -36,22 +37,3 @@ class Car:
 		else:
 			print("You can't add negative miles to an odometer!")
 
-my_new_car = Car('audi', 'a4', 2019)
-print(my_new_car.get_descriptive_name())
-
-#set the value of the odometer_reading attribute directly 
-my_new_car.odometer_reading = 23
-my_new_car.read_odometer()
-
-#set the value of the odometer_reading attribute through a method 
-my_new_car.update_odometer(22)
-my_new_car.update_odometer(24)
-my_new_car.read_odometer()
-
-my_used_car = Car('subaru', 'outback', 2015)
-print(my_used_car.get_descriptive_name())
-my_used_car.update_odometer(23_500)
-my_used_car.read_odometer()
-my_used_car.increment_odometer(-100)
-my_used_car.increment_odometer(100)
-my_used_car.read_odometer()
