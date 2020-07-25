38f757464735f4682730784e2b567add0fe87c29
giggly <giggly@abv.bg>
2020-07-25 23:04:46 +0300

    Exercises in reading a file.

---
+++ 10. Files and Exceptions/learning_python.py
@@ -0,0 +1,30 @@
+#10-1. Learning Python:Open a blank file in your text editor and write a few 
+#lines summarizing what you’ve learned about Python so far. Start each line 
+#with the phrase In Python you can. . . . Save the file as learning_python.txt in 
+#the same directory as your exercises from this chapter. Write a program that 
+#reads the file and prints what you wrote three times. Print the contents once by 
+#reading in the entire file, once by looping over the file object, 
+#and once by storing the lines in a list and then working with them outside the with block.
+
+filename = 'text_files\\learning_python.txt'
+#read the file
+with open(filename) as file_object:
+	contents = file_object.read()
+
+print(contents)
+print()
+
+#read the file line by line
+with open(filename) as file_object:
+	for line in file_object:
+		print(line.strip())
+print()
+
+#read the file and store it in a list
+lines = []
+with open(filename) as file_object:
+	for line in file_object:
+		lines.append(line.strip())
+
+for line in lines:
+	print(line)
