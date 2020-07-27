ecf98cb63d41985874119b6ff02422480f1c512a
giggly <giggly@abv.bg>
2020-07-27 21:53:17 +0300

    Writing multiple lines.

---
+++ 10. Files and Exceptions/write_messages.py
@@ -0,0 +1,12 @@
+file_name = 'text_files\\programming.py'
+
+#create and write to the file
+with open(file_name, 'w') as file_object:
+	file_object.write('I love programming.\n')	#use a new line
+	file_object.write('I love creating new games.')
+
+#open the file and check the content
+with open(file_name) as file_object:
+	content = file_object.read()
+print(content)
+
