86dd81adde44751570687139eedd710e455f8680
giggly <giggly@abv.bg>
2020-08-02 21:30:12 +0300

    Analyzing text.

--- 10. Files and Exceptions/alice.py
+++ 10. Files and Exceptions/alice.py
@@ -1,7 +1,10 @@
-filename = 'alice.txt'
+filename = 'text_files\\alice.txt'
 
 try:
 	with open(filename, encoding='utf-8') as f:
 		contents = f.read()
 except FileNotFoundError:
 	print(f"Sorry, the file {filename} does not exist.")
+else:
+	words_count = len(contents.split())
+	print(f"The file {filename} has about {words_count} words.")
